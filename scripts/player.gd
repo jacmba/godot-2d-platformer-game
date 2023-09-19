5d72e337252472769dc713bae58ac512f7531186
Jacinto Mba Cantero <jacmba@gmail.com>
2023-09-19 17:05:14 +0100

    Fixed movement. Custom controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,39 @@
 extends CharacterBody2D
 
-@export var speed = 400
-@export var jump_force = 400
-@export var gravity = 200
+const speed = 3000
+const jump_force = 250
+const gravity = 600
 
 var h_velocity
+var motion: float
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("idle")
+	motion = 0
+	
+func _process(_delta):
+	if Input.is_action_pressed("move_right"):
+		motion = lerp(motion, 1.0, 0.1)
+	elif Input.is_action_pressed("move_left"):
+		motion = lerp(motion, -1.0, 0.1)
+	else:
+		motion = lerp(motion, 0.0, 0.1)
+		
+	if abs(motion) > 0:
+		$Sprite2D.flip_h = motion > 0
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.play("idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	velocity.x = Input.get_axis("ui_left", "ui_right") * speed
-	
-	if abs(velocity.x) > 0:
-		$Sprite2D.flip_h = velocity.x > 0
-	
+func _physics_process(delta):	
+	velocity.x = motion * speed * delta
 	velocity.y += delta * gravity
 	
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
