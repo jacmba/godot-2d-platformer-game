97a7572f8b6ceeda9ce6f2d65839e0be771109eb
Jacinto Mba Cantero <jacmba@gmail.com>
2023-09-21 01:21:14 +0100

    Coin shader. Change stage when all coins collected

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,12 +1,18 @@
 extends Node
 
+@export var coin_list: Node
+
 var coins = 0
 
 @onready var coin_counter: Label = $CoinCounter
+@onready var stage_coins = coin_list.get_child_count()
 
 func _on_coin_collected():
 	coins += 1
 	coin_counter.text = str(coins)
+	
+	if coins == stage_coins:
+		get_tree().change_scene_to_file("res://scenes/world/stage.tscn")
 
 func _on_dead_zone_entered():
 	get_tree().reload_current_scene()
